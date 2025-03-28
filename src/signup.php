935f750b2e88b9c6415369862d0c302ac9fc2fa2
Gustavo <gustavoimp14@gmail.com>
2025-03-28 18:09:32 -0500

    cree el mensaje de error en caso de email repetido

--- src/signup.php
+++ src/signup.php
@@ -6,18 +6,38 @@
     $lname      = $_POST['l_name'];
     $email      = $_POST['mail'];
     $passwd    = $_POST['pass'];
+    $enc_pass=md5($passwd);
 
-    $sql = "INSERT INTO users (firstname, lastname, email, password)
-            VALUES('$fname','$lname','$email','$passwd')
-    
+    $sql_mail_validation = "
+    SELECT
+        COUNT(email) as total
+    FROM
+        users
+        WHERE email = '$email'
+        LIMIT 1
     ";
-
-    $res = pg_query($conn, $sql);
+    $res = pg_query($conn, $sql_mail_validation);
 
     if ($res){
-        echo "User has been created succesfully";
-    }else{
-        echo "Error suka blyat";
+        $row = pg_fetch_assoc($res);
+        if($row['total'] > 0){
+
+            echo " Cyka blyat! ya tienes una cuenta con ese correo";
+
+        }else{
+            $sql = "INSERT INTO users (firstname, lastname, email, password)
+                VALUES('$fname','$lname','$email','$enc_pass')
+    
+            ";
+
+            $res = pg_query($conn, $sql);
+
+            if ($res){
+                echo "User has been created succesfully";
+            }else{
+            echo "Cyka blyat, un error";
+            }
+        }
     }
     
 ?>
